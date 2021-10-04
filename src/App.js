cda3d5c46d75219d3feaadc66d9ae9f04872d935
Rakib602 <rakib0063602@gmail.com>
2021-10-04 23:37:55 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './Components/Home/Home';
-import Details from './Components/Details/Details';
 import Blog from './Components/Blog/Blog';
 import About from './Components/About/About';
 import Error from './Components/Error/Error';
